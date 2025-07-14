8f647a63c42776706e0f80de72479e6ff0ee174e
ねこがくれ <osjin83@gmail.com>
2025-07-14 17:11:39 +0900

    gui.h: reorganize struct CGUIState definition and include necessary headers for Windows

--- include/interface/gui.h
+++ include/interface/gui.h
@@ -2,10 +2,21 @@
 #define _INTERFACE_GUI_H
 
 #ifdef _WIN32
+#include <windows.h>
+#include <tchar.h>
+#include <commctrl.h>
+#include <wingdi.h>
+
+struct CGUIState{
+    unsigned int y;
+    unsigned int x;
+    unsigned int width;
+    unsigned int height;
+    char title[256];
+}typedef CGUIState;
 
 #else
 #include <X11/Xlib.h>
-
 
 struct CGUIState{
     unsigned int y;
@@ -27,6 +38,5 @@ struct CGUIState{
 * @return CGUIState pointer on success, NULL on failure
 */
 CGUIState *CCreateGUI(int x, int y, unsigned int width, unsigned int height, const char *title);
-
 
 #endif /* _INTERFACE_GUI_H */
